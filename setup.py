9d9efb5a858b845365fe7edecac1da1419c40e92
Uri Yallow <uriyallow14@gmail.com>
2025-03-23 20:30:15 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Uri',
+    author_email='uriyallow14@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn']
+    
+)
